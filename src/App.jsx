8577b2eb26c03d5b27fb4077a1a2f4313071dcd5
pranith kumar pendota <145289341+pranithkumar01@users.noreply.github.com>
2024-09-19 02:00:42 +0530

    import nitification

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
-import Notification from "./components/notification/Notification";
+import Notification from "./components/notification/Notification.jsx";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
 import { useUserStore } from "./lib/userStore";
